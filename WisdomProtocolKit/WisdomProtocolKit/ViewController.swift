6e3cbaef16e79f3ee68b9caaf806818e7c49628f
tangjianfengVS <497609288@qq.com>
2021-12-14 18:54:02 +0800

    projiect create

---
+++ WisdomProtocolKit/WisdomProtocolKit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WisdomProtocolKit
+//
+//  Created by tangjianfeng on 2021/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

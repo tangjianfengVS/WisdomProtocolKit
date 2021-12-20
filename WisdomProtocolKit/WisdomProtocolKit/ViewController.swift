1ce9af397f57be18acdfda4a36a61dc43d9bc849
tangjianfengVS <497609288@qq.com>
2021-12-20 10:52:11 +0800

    no message

--- WisdomProtocolKit/WisdomProtocolKit/ViewController.swift
+++ WisdomProtocolKit/WisdomProtocolKit/ViewController.swift
@@ -8,40 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let btn = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.white
+        view.backgroundColor = UIColor(white: 0.8, alpha: 1)
         // Do any additional setup after loading the view.
+        
+        // WisdomProtocolKit: 路由控制器跳转
+        view.addSubview(btn)
+        
+        btn.frame = CGRect.init(x: 30, y: 80, width: UIScreen.main.bounds.width-60, height: 40)
+        
+        btn.layer.borderColor = UIColor.red.cgColor
+        
+        btn.layer.borderWidth = 2
+        
+        btn.setTitle("WisdomProtocolKit: 路由控制器跳转", for: .normal)
+        
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        
+        btn.setTitleColor(UIColor.red, for: .normal)
+        
+        btn.addTarget(self, action: #selector(clickRoutorVCBtn), for: .touchUpInside)
     }
 
-
-}
-
-
-
-class TwoViewController: UIViewController, WisdomRegistProtocol {
     
-    static func registerProtocolClass() {
-        let model = WisdomProtocolConfigModel(configClass: self, configProtocol: TwoViewVCProtocol.self)
-
-        WisdomProtocolKit.registProtocolClassConfig(configs: [model]) { errorList in
-            print(errorList)
+    
+    @objc func clickRoutorVCBtn() {
+        
+        let routerClass: AnyClass? = WisdomProtocolKit.classFromProtocol(fromProtocol: TwoViewVCProtocol.self)
+        if let resClass = routerClass as? WisdomProtocolController.Type {
+            
+            resClass.wisdomProtocolControllerCla(rootVC: self, data: UIColor.white)
         }
     }
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        view.backgroundColor = UIColor.white
-        // Do any additional setup after loading the view.
-    }
-
 }
 
 
-@objc protocol TwoViewVCProtocol {
-    
-}
+
+
